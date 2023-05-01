b176b88851e069ede56d83ca0875fea036b02b4f
Sosmunk <dronoldino@gmail.com>
2023-05-01 23:32:20 +0500

    details service stub

--- account_service/src/main/java/com/skblab/project/account_service/service/DatabaseAccountDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.skblab.project.account_service.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class DatabaseUserDetailsService implements UserDetailsService {
-    private 
-}
