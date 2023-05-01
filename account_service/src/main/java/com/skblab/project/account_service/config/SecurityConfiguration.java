69fd1efe153e546ad31f2fb0c069402a4e17cea0
Sosmunk <dronoldino@gmail.com>
2023-05-01 23:19:19 +0500

    jdbc auth

--- account_service/src/main/java/com/skblab/project/account_service/config/SecurityConfiguration.java
+++ account_service/src/main/java/com/skblab/project/account_service/config/SecurityConfiguration.java
@@ -1,4 +1,17 @@
 package com.skblab.project.account_service.config;
 
 public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .authoritiesByUsernameQuery("select PHONE, ROLE from account where PHONE=?")
+                .usersByUsernameQuery("select PHONE, PASSWORD, ENABLED from account where USERNAME=?");
+    }
+
 }
