dbec111af906add7af4005298b1242870fd855c3
Sosmunk <dronoldino@gmail.com>
2023-05-01 23:32:04 +0500

    jdbc auth

--- account_service/src/main/java/com/skblab/project/account_service/config/SecurityConfiguration.java
+++ account_service/src/main/java/com/skblab/project/account_service/config/SecurityConfiguration.java
@@ -1,6 +1,20 @@
 package com.skblab.project.account_service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    DataSource dataSource;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -10,8 +24,7 @@ public class SecurityConfiguration {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .authoritiesByUsernameQuery("select PHONE, ROLE from account where PHONE=?")
-                .usersByUsernameQuery("select PHONE, PASSWORD, ENABLED from account where USERNAME=?");
+                .authoritiesByUsernameQuery("select ACCOUNT_ID, ROLE from account where PHONE=?")
+                .usersByUsernameQuery("select ACCOUNT_ID, PASSWORD, ENABLED from account where USERNAME=?");
     }
-
 }
